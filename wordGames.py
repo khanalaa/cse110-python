ea9b38a072cdf782dc7ee00b11386f3d84a1da62
khanalaa <kaayush524@gmail.com>
2023-09-19 17:17:39 -0600

    done

--- wordGames.py
+++ wordGames.py
@@ -1,9 +1,22 @@
-print("Please enter the following:")
+print("Please enter the following:\n")
 adjective = input("adjective:")
 animal = input("animal:")
 verb0 = input("verb:")
 exclamation = input("exclamation:")
 verb1 = input ("verb:")
 verb2 = input ("verb:")
+a_an = "a"
+print()
+ 
+x = animal[0] 
+if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u'):
+    a_an = "an" 
+    
 
-print("Your story is:")
+print("Your story is:\n")
+print("Within a secluded forest, a " + adjective + " figure, \
+cloaked in baggy attire, " + verb0 + " toward a \
+concealed box. It was "+ a_an +' '+ animal +  " and " +verb2 +" off \
+through the door. \"" + exclamation + "!\" I yelled. With a " + adjective + " grin,\
+they " + verb1 + " through, setting off on an extraordinary \
+journey across time and space.")
