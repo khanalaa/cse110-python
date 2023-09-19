a8637b9d695f474a6f0c4f0e9ef912416a6a0cf4
khanalaa <kaayush524@gmail.com>
2023-09-19 14:39:57 -0600

    edited a bit

---
+++ wordGames.py
@@ -0,0 +1,9 @@
+print("Please enter the following:")
+adjective = input("adjective:")
+animal = input("animal:")
+verb0 = input("verb:")
+exclamation = input("exclamation:")
+verb1 = input ("verb:")
+verb2 = input ("verb:")
+
+print("Your story is:")
