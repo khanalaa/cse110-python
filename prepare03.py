60ccf2bbbb8fb3e6a0ae15192d0969850b2dc57e
khanalaa <kaayush524@gmail.com>
2023-09-25 20:20:51 -0600

    week 3 assignment 1

---
+++ prepare03.py
@@ -0,0 +1,11 @@
+age = input("How old are you? ")
+next_age = int(age) + 1
+print("On your next birthday, you will be " + str(next_age))
+
+cartons = input("\nHow many egg cartons do you have? ")
+eggs = int(cartons) * 12
+print ("You have "+ str(eggs) + " eggs")
+
+cookie = int(input("\nHow many cookies do you have? "))
+people = int(input("How many cookies do you have? "))
+print("Each person may have " + str(cookie/people) + " cookies\n")
