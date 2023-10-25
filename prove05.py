76f7c7f66db022b972d329d60a937d9fe34fd54b
khanalaa <kaayush524@gmail.com>
2023-10-25 14:57:55 -0600

    assignment

--- prove05.py
+++ prove05.py
@@ -30,21 +30,25 @@ You see a large grizzly bear, and then the match burns out. Do you want to RUN, 
         choice_run = input("\nYou can either head for the RIVER, hoping water deters the bear, or climb the HILL to gain elevation and evade it.\n").lower()
         if (choice_run == "hill"):
             print("\nGame Over!!! From the top of the hill you fell down.")
-        if (choice_run == "river"):
+        elif (choice_run == "river"):
             print("\nGame Over!!! You wade into the cold water, and the bear hesitates, but you drowned.")
+        else:
+            print("\nGame Over!!! Wrong input")
     if (choice_m == "hide"):
         choice_hide = input("You can either hide in the CAVE, hoping the bear won't find you, or climb the TREE, hoping it can't reach you.\n").lower()
         if (choice_hide == "cave"):
             print("\nGame Over!!! The bear finds you. And you are dead.")
-        if(choice_hide == "tree"):
+        elif(choice_hide == "tree"):
             print("\nGame Over!!! You fell off the tree.")
+        else:
+             print("\nGame Over!!! Wrong input")
 
 elif (choice1 == "flashlight"):
     choice_f = input("You pick up the flashlight and turn it on. You see the pathway lit up in front of you, \
 but you thought you also heard something off to the side. Do you want to FOLLOW the path or LOOK in the trees \
 for the thing that made the noise?\n").lower()
     if choice_f == "follow":
-        choice_follow = input(" You decide to follow the path, flashlight piercing the darkness. The safe, marked trail leads deeper into the forest. CONTINUE, STOP, or EXAMINE?\n").lower()
+        choice_follow = input("You decide to follow the path, flashlight piercing the darkness. The safe, marked trail leads deeper into the forest. CONTINUE, STOP, or EXAMINE?\n").lower()
         if choice_follow == "continue":
             print("\nYou win!!! You proceed along the path, finding a serene clearing under the starry sky.")
         elif choice_follow == "stop":
@@ -61,6 +65,7 @@ for the thing that made the noise?\n").lower()
             print("\nGame Over!!! You cautiously approach but cannot win the battle with bear. ")
         elif choice_look == "shine":
             print("\nGame Over!!! You use your flashlight to check for other animals and saw tiger.")
-            
+        else:
+            print("\nGame Over!!! Wrong input")
 else:
     print("\nGame Over!!! You are dead")
---
+++ prove07.py
@@ -0,0 +1,46 @@
+secret_word = "temple"
+
+print("Welcome to the guessing game!!!")
+
+count = 0
+for letter in secret_word:
+    count += 1
+
+print("\nYour hint is: " + ("_ "*count))
+
+display = []
+guess = input("What is your guess? ")
+guess_count = 0
+for char in guess:
+    guess_count += 1
+    display += "_"
+
+
+is_true = False
+loop_count = 0
+
+while not is_true :
+    loop_count += 1
+    if len(guess) != count:
+        print("\nSorry, the guess must have the same number of letters as the secret word.")
+        guess = input("What is your guess? ")
+    else:
+        if secret_word == guess:
+            print("\nCongratulations! You guessed it!")
+            is_true = True
+        else:
+            position = 0
+            for char in guess:
+                if char == secret_word[position]:
+                    display[position] = char.upper()
+                else:
+                    for i in secret_word:
+                        if i == char:
+                            display[position] = char
+                position += 1
+
+            print(f"\nYour hint is:{' '.join(display)}")
+            guess = input("What is your guess? ")
+    
+    
+print(f"It took you {loop_count} guesses.")
