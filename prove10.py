913d29c5d1f92aefae2941f79bce7df225f8f85b
khanalaa <kaayush524@gmail.com>
2023-11-14 12:59:44 -0700

    done

--- prove10.py
+++ prove10.py
@@ -1,6 +1,8 @@
+# NAME: Aayush Khanal
+
 print("Welcome to the Shopping Cart Program!\n")
 
-print('''Please select one of the following:\n 
+print('''Please select one of the following:
 1. Add item
 2. View cart
 3. Remove item
@@ -10,32 +12,39 @@ print('''Please select one of the following:\n
 
 item = []
 prices = []
+quantities = []
+total = []
 choice = int(input("Please enter an action: "))
 
 while choice != 5:
     if choice == 1:
         item_name = input("What item would you like to add? ")
         price = float(input(f"What is the price of '{item_name}'? "))
-        print(f"'{item_name}' has been added to the cart.")
+        quantity = int(input(f"Enter the number of {item_name}. "))
+        print(f"{quantity} '{item_name}' has been added to the cart.\n")
         item.append(item_name)
         prices.append(price)
+        quantities.append(quantity)
+        total.append(price * quantity)
 
     elif choice == 2:
         print("The contents of the shopping cart are:")
-        for index, (item_name,price) in enumerate(zip(item, prices)):
-            print(f"{index + 1}. {item_name} - ${price}\n")
+        for index, (item_name,price,quantity) in enumerate(zip(item, prices,quantities)):
+            print(f"{index + 1}. {quantity} {item_name} - ${price} each\n")
 
     elif choice == 3:
         remove = int(input("Which item would you like to remove? ")) - 1
         item.pop(remove)
         prices.pop(remove)
-        print("Item removed.")
+        quantities.pop(remove)
+        total.pop(remove)
+        print("Item removed.\n")
 
     elif choice == 4:
-        total = sum(prices)
-        print(f"The total price of items in the cart is: ${total}")
+        final_total = sum(total)
+        print(f"The total price of items in the cart is: ${final_total}\n")
 
-    print('''Please select one of the following:\n 
+    print('''Please select one of the following:
 1. Add item
 2. View cart
 3. Remove item
