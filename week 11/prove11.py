6884eaf841957fb05976907a70a4692b7640e281
khanalaa <kaayush524@gmail.com>
2023-11-21 16:19:49 -0700

    hyg

--- week 11/prove11.py
+++ week 11/prove11.py
@@ -1,2 +1,12 @@
+
 import csv
+
+# year = int(input("Enter the year of interest: "))
+
+with open("life-expectancy.csv", 'r') as data:
+    read = csv.reader(data)
+
+    for line in read:
+        print(line)
+    
 
